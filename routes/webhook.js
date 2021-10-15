59c853f11d9bf821051bc6856f276213125a89f6
Antón Molleda <amolleda@gmail.com>
2021-10-15 13:54:31 -0700

    feat: new event type
    This change addresses 2 changes:
    
    1. Removed the handling of a new release. Currently we do not seem to be
       processing correctly that event (or even receiving it). Because we do
       not have access to the payloads it is faster to work with `push`
       instead of debugging because we are going to have versioned docs.
    2. The other change is to add the `event_type` `doc_changes_previous`.
       This type is used when the `push` happens in a branch that is not the
       latest stable one. Additionally it changes the logic to find the
       latest version by using a GraphQL query to get the latest releases
       instead of relying on npm. The reason is that there is a delay of
       up to 2 hours between a GitHub release and the moment it is published
       in npm.

--- routes/webhook.js
+++ routes/webhook.js
@@ -6,12 +6,15 @@ const {
   getLatestInformation,
   verifyIntegrity,
   sendRepositoryDispatchEvent,
-  getSHAFromTag,
 } = require('../utils/utils');
 
 const OWNER = process.env.OWNER || 'electron';
 const SOURCE_REPO = `electron`;
 const TARGET_REPO = `electronjs.org-new`;
+const EVENT_TYPE = {
+  CURRENT: 'doc_changes',
+  PREVIOUS: 'doc_changes_previous',
+};
 
 /**
  * Verifies there is at least one file added, modified, or removed
@@ -37,23 +40,55 @@ const areFilesInFolderChanged = (pushEvent, folder) => {
 };
 
 /**
+ * Compares 2 refs or branches and returns a boolean indicating
+ * if `current` is from a previous release than `latest`.
+ * @param {string} latest
+ * @param {string} current
+ */
+const getEventType = (latest, current) => {
+  try {
+    const majorRegex = /(?:refs\/heads\/)?(\d+)-x-y/;
+    const [, latestMajor] = majorRegex.exec(latest);
+    const [, currentMajor] = majorRegex.exec(current);
+
+    const parsedLatest = parseInt(latestMajor);
+    const parsedCurrent = parseInt(currentMajor);
+
+    if (parsedCurrent < parsedLatest) {
+      return EVENT_TYPE.PREVIOUS;
+    } else {
+      return EVENT_TYPE.CURRENT;
+    }
+  } catch (e) {
+    return '';
+  }
+};
+
+/**
  * Handler for the GitHub webhook `push` event.
  * @param {import('express').Request} req
  * @param {import('express').Response} res
  */
 const pushHandler = async (req, res) => {
   const { branch } = await getLatestInformation();
-  const ref = `refs/heads/${branch}`;
 
   /** @type {import('@octokit/webhooks-types').PushEvent} */
   const payload = req.body;
 
   if (
-    payload.ref === ref &&
     payload.repository.full_name === `${OWNER}/${SOURCE_REPO}` &&
     areFilesInFolderChanged(payload, 'docs')
   ) {
-    await sendRepositoryDispatchEvent(OWNER, TARGET_REPO, payload.after);
+    const eventType = getEventType(branch, payload.ref);
+    if(!eventType){
+      console.log(`Could not find right version for ${payload.ref}`)
+      return;
+    }
+
+    await sendRepositoryDispatchEvent(OWNER, TARGET_REPO, eventType, {
+      sha: payload.after,
+      branch: payload.ref.replace('refs/heads/', ''),
+    });
   }
 
   return res.status(200).send();
@@ -61,45 +96,14 @@ const pushHandler = async (req, res) => {
 
 /**
  * Handler for the GitHub webhook `release` event.
- * The payload will be processed only if the release
- * payload is for the stable branch.
  * @param {import('express').Request} req
  * @param {import('express').Response} res
  */
 const releaseHandler = async (req, res) => {
+  // We keep this handler for historic reasons but all updates are done via the push event
+
   console.log(`New release payload received`);
-  /** @type {import('@octokit/webhooks-types').ReleaseEvent} */
-  const payload = req.body;
 
-  const { version } = await getLatestInformation();
-
-  // Tags can be v14.0.0-nightly.20210504, v13.0.0-beta.21, v10.4.5, etc.
-  // We only want to process the stable ones, i.e.: v10.4.5
-  // so we remove the initial `v` and we "clean it". If the cleaned
-  // string is the same as before, then it's a stable release.
-  // We also check that the new release is greater or equal than the
-  // published npm version. There can be 30-120s delay between a GitHub
-  // release and an npm one.
-  const tag = payload.release.tag_name.replace(/^v/, '');
-  const isStable = semver.coerce(tag).version === tag;
-
-  console.log(`Version received: ${tag}`);
-  console.log(`Latest version:   ${version}`);
-
-  if (
-    payload.action === 'released' &&
-    !payload.release.draft &&
-    !payload.release.prerelease &&
-    isStable &&
-    semver.gte(tag, version)
-  ) {
-    const sha = await getSHAFromTag(
-      payload.repository.full_name,
-      payload.release.tag_name
-    );
-
-    await sendRepositoryDispatchEvent(OWNER, TARGET_REPO, sha);
-  }
   return res.status(200).send();
 };
 /**
--- utils/utils.js
+++ utils/utils.js
@@ -1,10 +1,9 @@
-const latestVersion = require('latest-version');
 const { graphql } = require('@octokit/graphql');
 const { verify } = require('@octokit/webhooks-methods');
 const { Octokit } = require('@octokit/rest');
 const { createAppAuth } = require('@octokit/auth-app');
+const { compare } = require('semver');
 
-const DOC_CHANGES_TYPE = 'doc_changes';
 const {
   GITHUB_TOKEN,
   WEBHOOK_SECRET,
@@ -15,12 +14,69 @@ const {
   CLIENT_SECRET,
 } = process.env;
 
+/**
+ * @param {NodeResult} release
+ */
+const getVersion = (release) => {
+  /**
+   * Electron releases urls have a well-known format. E.g.:
+   * https://github.com/electron/electron/releases/tag/v14.0.0-beta.2
+   * We only need the latest part (i.e. `14.0.0-beta.2`)
+   */
+  return release.url.split('/v').pop();
+};
+
+/**
+ * Transforms a NodeResult into a Release
+ * @param {NodeResult[]} releases
+ */
+const toReleases = (releases) => {
+  const stables = releases.filter((release) => !release.isPrerelease);
+
+  const versions = stables.map((release) => {
+    return getVersion(release);
+  });
+
+  return versions;
+};
+
+const getReleases = async () => {
+  const query = `
+{
+  repository(owner: "electron", name: "electron") {
+    refs(refPrefix: "refs/tags/", first: 1) {
+      nodes {
+        repository {
+          releases(first: 100, orderBy: {field: CREATED_AT, direction: DESC}) {
+            nodes {
+              name
+              url
+              isPrerelease
+            }
+          }
+        }
+      }
+    }
+  }
+}`;
+
+  const graphqlWithAuth = await getAuthenticatedGraphql();
+  const queryResults = await graphqlWithAuth(query);
+
+  const releases = toReleases(
+    queryResults.repository.refs.nodes[0].repository.releases.nodes
+  );
+
+  return releases.sort(compare);
+};
+
 const getLatestInformation = async () => {
-  const version = await latestVersion('electron');
-  const branch = version.replace(/\.\d+\.\d+$/, '-x-y');
+  const releases = await getReleases();
+  const latestVersion = releases.pop();
+  const branch = latestVersion.replace(/\.\d+\.\d+$/, '-x-y');
 
   return {
-    version,
+    version: latestVersion,
     branch,
   };
 };
@@ -107,25 +163,24 @@ const getAuthorization = () => {
 
 /**
  * Sends a `repository_dispatch` event top the given repo `target`
- * with the type `doc_changes` and the given commit `sha` as part
- * of the payload.
+ * with the given type and payload.
  * @param {string} owner The owner of the repo to send the event to
  * @param {string} repo The repo to send the event to
- * @param {string} sha The commit's SHA
+ * @param {string} eventType The type of event_dispatch to use
+ * @param {object} payload The event's payload
  */
-const sendRepositoryDispatchEvent = async (owner, repo, sha) => {
+const sendRepositoryDispatchEvent = async (owner, repo, eventType, payload) => {
   const octokit = new Octokit(getAuthorization());
 
-  console.log(`Sending payload with SHA "${sha}"`);
+  console.log(`Sending payload (${eventType}):
+${JSON.stringify(payload, null, 2)}`);
 
   try {
     await octokit.repos.createDispatchEvent({
       owner,
       repo,
-      event_type: DOC_CHANGES_TYPE,
-      client_payload: {
-        sha,
-      },
+      event_type: eventType,
+      client_payload: payload,
     });
 
     console.log(`Payload sent`);
@@ -154,54 +209,8 @@ const getAuthenticatedGraphql = async () => {
   }
 };
 
-/**
- * For a given `tagName`, returns the associated SHA.
- *
- * @param {string} repository The repository in the form of `owner/name`
- * @param {string} tagName
- * @returns {Promise<string>}
- */
-const getSHAFromTag = async (repository, tagName) => {
-  console.log(`Getting SHA for "${repository}" and "${tagName}"`);
-
-  const [owner, repo] = repository.split('/');
-
-  const parameters = {
-    owner,
-    repo,
-    tagName,
-  };
-
-  const graphqlWithAuth = await getAuthenticatedGraphql();
-
-  const {
-    repository: {
-      release: {
-        tagCommit: { oid },
-      },
-    },
-  } = await graphqlWithAuth(
-    `
-      query shaFromTag($owner: String!, $repo: String!, $tagName: String!) {
-        repository(owner: $owner, name: $repo) {
-          release(tagName: $tagName) {
-            tagCommit {
-              oid
-            }
-          }
-        }
-      }
-    `,
-    parameters
-  );
-
-  console.log(`SHA is ${oid}`);
-  return oid;
-};
-
 module.exports = {
   getLatestInformation,
-  getSHAFromTag,
   sendRepositoryDispatchEvent,
   verifyIntegrity,
 };
